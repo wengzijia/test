490150e9884144128c04de96d1c1791ad5857795
FELaoL <felaol@sina.com>
2022-08-15 11:28:31 +0800

    444

---
+++ tetst.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name:123,
+      worods:"357889"
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
