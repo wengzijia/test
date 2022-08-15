5318b17b6b410daaa64edc5697c36722d8aa35e7
FELaoL <felaol@sina.com>
2022-08-15 11:32:08 +0800

    1111

--- tetst.vue
+++ tetst.vue
@@ -7,10 +7,10 @@ export default {
   data(){
     return{
       name:12223332,
-      worods:"357884656",
-      worods1:"357884656",
-      worods12:"二号位和",
-      worods33:"3无法击败v",
+      worods:"3578846560000",
+      worods1:"3578846560000",
+      worods12:"二号位和000",
+      worods33:"3无法击败0000v",
     }
   }
 
