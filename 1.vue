ffef8ac5bdddbfc30ae3156527165a0c23bff94d
wengzijia <1486612633@qq.com>
2022-08-15 12:25:20 +0800

    1

--- 1.vue
+++ 1.vue
@@ -4,7 +4,7 @@
           urlagddgd: '/price/index2?cateId=100&subCateId=38&parts=1',
           namgdgdsge: '珍珠棉内托1111111',
           isShowgsdgdsgds: false,
-          cateIdfdgdsgsd: 100,
-          subCateId: 38,
-          goodsInfo: { totalPrice: 0 }
+          catedgdgdIdfdgdsgsd: 100,
+          subCatedgggdsdId: 38,
+          goodsInfosfasd: { totalPrice: 0 }
         },
