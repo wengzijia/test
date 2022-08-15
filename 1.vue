b1fa75f69d2585ca54fb7a37b527a6e56d6f9934
wengzijia <1486612633@qq.com>
2022-08-15 11:28:15 +0800

    1

--- 1.vue
+++ 1.vue
@@ -2,7 +2,7 @@
         {
           id: 'iframe_100_38',
           url: '/price/index2?cateId=100&subCateId=38&parts=1',
-          name: '珍珠棉内托',
+          name: '珍珠棉内托1111111',
           isShow: false,
           cateId: 100,
           subCateId: 38,
