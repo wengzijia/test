83d3bc31adafe28cb79e2b00efe070d7154b33d9
wengzijia <1486612633@qq.com>
2022-08-15 11:35:28 +0800

    1

--- 1.vue
+++ 1.vue
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-        {
-          id: 'iframe_100_38',
-          url: '/price/index2?cateId=100&subCateId=38&parts=1',
-          name: '珍珠棉内托3123232',
-          isShodgfhffw: false,
-=======
-
         {
           id: 'iframe_100_38',
           url: '/price/index2?cateId=100&subCateId=38&parts=1',
