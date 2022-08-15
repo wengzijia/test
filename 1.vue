c10ebf30487618c0a6390652f8b893314db0dc17
FELaoL <felaol@sina.com>
2022-08-15 12:00:19 +0800

    3

--- 1.vue
+++ 1.vue
@@ -1,10 +1,10 @@
 
         {
-          idgdsgdsdtrtrsgg1: 'iframe_100_38',
-          urlagddtrtrgd: '/price/index2?cateId=100&subCateId=38&parts=10000',
-          namgdgdstrtge: '珍珠棉内托11111100000011111111',
-          isShowgsdtrtgdsgds: false,
-          cateIdfdtrgdsgsd: 10000,
+          idgdsgdsdt44rtrsgg1: 'iframe_100_38',
+          urlagddtr44trgd: '/price/index2?cateId=100&subCateId=38&parts=10000',
+          namgdgdst444rtge: '珍珠棉内托11111100000011111111',
+          isShowgsdt44rtgdsgds: false,
+          cateIdfdtrgd444sgsd: 10000,
           subCatetrId: 38,
-          goodsIntrfo: { totalPrice: 6 }
+          goodsIntrfo: { tot444alPrice: 6 }
         },
