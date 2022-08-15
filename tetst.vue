fd35d646d4e0b2586f279de4ef2bf3e0deef0aee
FELaoL <felaol@sina.com>
2022-08-15 11:48:26 +0800

    1

--- tetst.vue
+++ tetst.vue
@@ -6,14 +6,13 @@
 export default {
   data(){
     return{
-      name:12223332,
+      nhdhsdhhsdame:12223332,
       worods:"3578846560000",
-      worods1:"3578846560000",
-      worods12:"二号位和000",
-      worods33:"3无法击败0000v",
+      worosdhshshfds1:"3578846560000",
+      wordsdhdshdhdshods12:"二号位和000",
+      worosdhsdfhddgsgss33:"3无法击败0000v",
     }
   }
-
 }
 </script>
 
