0f67c6961ea3fb6ce2bafeb9732ab2a51493310e
wengzijia <1486612633@qq.com>
2022-08-15 12:21:30 +0800

    1

--- tetst.vue
+++ tetst.vue
@@ -6,14 +6,6 @@
 export default {
   data(){
     return{
-<<<<<<<<< Temporary merge branch 1
-      data:113131131,
-      nhdhsdhsdgsddshsdame:12223332,
-      woroddshshfs:"3578846560000",
-      worosdhshshsfhshsfhfds1:"3578846560000",
-      wordsdhdshdhdsdhfshfhshods12:"二号位和000",
-      worosdhsdfhdddshfhffhgsgss33:"3无法击败0000v",
-=========
       dgdsgd:12223332,
       sdgsdg:"3578846560000",
       gsdgds:"3578846560000",
@@ -22,9 +14,7 @@ export default {
       gsdg:"3无法击败0000v",
       dgsdgdsgdg:"3无法击败0000v",
       worosdhsdgsdgsdgdssdfhddgsgss33:"3无法击败0000v",
->>>>>>>>> Temporary merge branch 2
     }
-      dsgdsgsdg:"ddddd",g
   }
 }
 </script>
