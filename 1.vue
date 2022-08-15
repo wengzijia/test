6da4881db9b7172716de4690942b31d12f24975f
wengzijia <1486612633@qq.com>
2022-08-15 12:26:56 +0800

    1

--- 1.vue
+++ 1.vue
@@ -2,9 +2,9 @@
         {
           idgdsgdsdtrtrsgg1: 'iframe_100_38',
           urlagddtrtrgd: '/price/index2?cateId=100&subCateId=38&parts=10000',
-          namgdgdstrtge: '珍珠棉内托11111100000011111111',
-          isShowgsdtrtgdsgds: false,
-          catedgdgdIdfdtrgdsgsd: 10000,
-          subCatetrdgggdsdId: 38,
-          goodsIntrfosfasd: { totalPrice: 6 }
+          namgdgdstrxzzzvxztge: '珍珠棉内托11111100000011111111',
+          isShowgsdtxvxzvxrtgdsgds: false,
+          catedgdgdIdfxzvxzvxdtrgdsgsd: 10000,
+          subCatetrdgggdsdvzxvxzId: 38,
+          goodsIntrfosfasdvzvxzvxzvx: { totalPrice: 6 }
         },
