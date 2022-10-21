046ac513972ab7f00a57a4e3e3305c33a1d2435a
wengzijia <1486612633@qq.com>
2022-10-21 11:09:21 +0800

    1

---
+++ 1.vue
@@ -0,0 +1,10 @@
+
+        {
+          idgdsgdsdtrtrsgg1: 'iframe_100111111',
+          urlagddtxvxzvxrtrgd: '/price/index2?cateId=100&subCateId=38&parts=10000',
+          namgdgdstrxzvzxvxtge: '珍珠棉内托11111100000011111111',
+          isShowgsdtrtgdsgds: false,
+          cateIdfdtrxzvzxvzgdsgsd: 10000,
+          subCatetrId: 38,
+          goodsIntrfo: { totalPrice: 6 }
+        },
